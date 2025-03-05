9991eda647051eaf14335dd9ab39ef96d7d9952a
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-03-05 17:37:19 +0300

    user olusturuldu ve baglandi

--- blog/urls.py
+++ blog/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('',views.index, name="index"),
     path('about/',views.about, name="about"),
     path('articles/',include("article.urls")),
-
+    path('user/',include("user.urls")),
 ##
 ]
---
+++ user/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def register(request):
+   return render(request,"register.html")
+def login(request):
+   return HttpResponse("login")
+def logout(request):
+   return HttpResponse("logout")
